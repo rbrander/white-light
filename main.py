3586c01f8331aec3e39806cdcec134d154aef990
Rob Brander <rob.brander@gmail.com>
2026-02-26 21:04:09 -0500

    added blue button

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ TODO:
   - add quit game
 - set icon (button image)
 - create a button class
-- add a blue button
 - add a way to upgrade to a square button
 - add a way to deactive a button (perhaps change alpha to indicate grayed out)
 """
@@ -44,24 +43,30 @@ def main():
 
   sprite_sheet = pygame.image.load("assets/push-buttons.png").convert_alpha()
 
-  green_round_btn_up_rect = pygame.Rect(35,  71, 118, 89)
-  green_round_btn_down_rect = pygame.Rect(172, 71, 118, 89)
   red_round_btn_up_rect = pygame.Rect(332,  71, 118, 89)
   red_round_btn_down_rect = pygame.Rect(469, 71, 118, 89)
+  green_round_btn_up_rect = pygame.Rect(35,  71, 118, 89)
+  green_round_btn_down_rect = pygame.Rect(172, 71, 118, 89)
+  blue_round_btn_up_rect = pygame.Rect(35,  343, 118, 89)
+  blue_round_btn_down_rect = pygame.Rect(172, 343, 118, 89)
 
-  green_btn_up = sprite_sheet.subsurface(green_round_btn_up_rect)
-  green_btn_down = sprite_sheet.subsurface(green_round_btn_down_rect)
   red_btn_up = sprite_sheet.subsurface(red_round_btn_up_rect)
   red_btn_down = sprite_sheet.subsurface(red_round_btn_down_rect)
+  green_btn_up = sprite_sheet.subsurface(green_round_btn_up_rect)
+  green_btn_down = sprite_sheet.subsurface(green_round_btn_down_rect)
+  blue_btn_up = sprite_sheet.subsurface(blue_round_btn_up_rect)
+  blue_btn_down = sprite_sheet.subsurface(blue_round_btn_down_rect)
 
-  green_btn_rect = green_btn_up.get_rect(center=(WIDTH//2, HEIGHT//2))
-  red_btn_rect = red_btn_up.get_rect(center=(WIDTH//2, HEIGHT//2 + 100))
+  red_btn_rect = red_btn_up.get_rect(center=(WIDTH//2, HEIGHT//2 + 0))
+  green_btn_rect = green_btn_up.get_rect(center=(WIDTH//2, HEIGHT//2 + 100))
+  blue_btn_rect = blue_btn_up.get_rect(center=(WIDTH//2, HEIGHT//2 + 200))
 
   running = True
   isButtonDown = False
-  downButtonColor: str | None = None # green, red, or None
+  downButtonColor: str | None = None # green, red, blue, or None
   while running:
-    showRedButton = score >= 10
+    showGreenButton = score >= 10
+    showBlueButton = score >= 100
     for event in pygame.event.get():
       match event.type:
         case pygame.QUIT:
@@ -73,16 +78,21 @@ def main():
             btn_up_sound.play()
         case pygame.MOUSEBUTTONDOWN:
           if event.button == 1:
-            if green_btn_rect.collidepoint(event.pos):
-              downButtonColor = 'green'
-              isButtonDown = True
-              btn_down_sound.play()
-              score += 1
-            elif showRedButton and red_btn_rect.collidepoint(event.pos):
+            if red_btn_rect.collidepoint(event.pos):
               downButtonColor = 'red'
               isButtonDown = True
               btn_down_sound.play()
+              score += 1
+            elif showGreenButton and green_btn_rect.collidepoint(event.pos):
+              downButtonColor = 'green'
+              isButtonDown = True
+              btn_down_sound.play()
               score += 10
+            elif showBlueButton and blue_btn_rect.collidepoint(event.pos):
+              downButtonColor = 'blue'
+              isButtonDown = True
+              btn_down_sound.play()
+              score += 100
 
     # clear background
     screen.fill(background_color)
@@ -90,14 +100,19 @@ def main():
     # guage
     screen.blit(left_guage, left_guage_dest_rect)
 
-    # green button
-    img = green_btn_down if isButtonDown and downButtonColor == 'green' else green_btn_up
-    screen.blit(img, green_btn_rect)
-
     # red button
-    if showRedButton:
-      img = red_btn_down if isButtonDown and downButtonColor == 'red' else red_btn_up
-      screen.blit(img, red_btn_rect)
+    img = red_btn_down if isButtonDown and downButtonColor == 'red' else red_btn_up
+    screen.blit(img, red_btn_rect)
+
+    # green button
+    if showGreenButton:
+      img = green_btn_down if isButtonDown and downButtonColor == 'green' else green_btn_up
+      screen.blit(img, green_btn_rect)
+
+    # blue button
+    if showBlueButton:
+      img = blue_btn_down if isButtonDown and downButtonColor == 'blue' else blue_btn_up
+      screen.blit(img, blue_btn_rect)
 
     # score text
     text_surface = font.render(f"Score: {score}", True, (0, 0, 0))
