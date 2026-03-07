43b430ff40475a963d4647b8fc7e063191e394e1
Rob Brander <rob.brander@gmail.com>
2026-03-07 01:13:05 -0500

    add time formatting util function

--- constants.py
+++ constants.py
@@ -10,3 +10,9 @@ LIGHT_COLOR = pygame.color.Color(239, 243, 228)
 DARK_COLOR = pygame.color.Color(60, 60, 60)
 DARK_BLUE = pygame.color.Color(0, 0, 100)
 DARK_PURPLE = pygame.color.Color(70, 0, 70)
+
+# e.g. 1d 2h 3m 4s = 1*86,400,000 + 2*3,600,000 + 3*60,000 + 4*1,000 = 93,784,000
+MS_PER_SECOND = 1000
+MS_PER_MINUTE = MS_PER_SECOND * 60 # 60_000
+MS_PER_HOUR = MS_PER_MINUTE * 60 # 3_600_000
+MS_PER_DAY = MS_PER_HOUR * 24 # 86_400_000
--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import sys
 import pygame
-from constants import HEIGHT, LIGHT_COLOR, WIDTH
+from constants import HEIGHT, LIGHT_COLOR, MS_PER_DAY, MS_PER_HOUR, MS_PER_MINUTE, MS_PER_SECOND, WIDTH
 
 def exit_program():
   pygame.quit()
@@ -17,3 +17,34 @@ def draw_grid_overlay(screen:pygame.Surface, x_gap:int = 50, y_gap:int = 100):
     pygame.draw.line(screen, LIGHT_COLOR, (x, 0), (x, HEIGHT), 2)
   for y in range(0, HEIGHT, y_gap):
     pygame.draw.line(screen, LIGHT_COLOR, (0, y), (WIDTH, y), 2)
+
+
+def format_time(time:int) -> list[str]:
+  formatted_strings = []
+  remaining = time
+
+  # days
+  num_days = remaining // MS_PER_DAY
+  if num_days > 0:
+    formatted_strings.append(f"{num_days} day{"s" if num_days > 1 else ""}")
+    remaining -= num_days * MS_PER_DAY
+
+  # hours
+  num_hours = remaining // MS_PER_HOUR
+  if num_hours > 0:
+    formatted_strings.append(f"{num_hours} hour{"s" if num_hours > 1 else ""}")
+    remaining -= num_hours * MS_PER_HOUR
+
+  # minutes
+  num_minutes = remaining // MS_PER_MINUTE
+  if num_minutes > 0:
+    formatted_strings.append(f"{num_minutes} minute{"s" if num_minutes > 1 else ""}")
+    remaining -= num_minutes * MS_PER_MINUTE
+
+  # seconds
+  num_seconds = remaining // MS_PER_SECOND
+  if num_seconds > 0:
+    formatted_strings.append(f"{num_seconds} second{"s" if num_seconds > 1 else ""}")
+    remaining -= num_seconds * MS_PER_SECOND
+
+  return formatted_strings
